ae9291462b00cb6fc6bae265b586f2def94fa001
jm360 <jm360@st-andrews.ac.uk>
2020-09-01 14:38:37 +0100

    Groundwork for initial NiLBS implementation.

--- NiLBS/skinning/skinning_mesh_lbs.py
+++ NiLBS/skinning/skinning_mesh_lbs.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 
-class LBSMeshDeformer:
+class LBSDeformer:
     """
 
 
@@ -11,29 +11,28 @@ class LBSMeshDeformer:
 
     """
 
-    def __init__(self, rest_vertices, weighting_function):
+    def __init__(self, weighting_function):
         """
 
-        :param rest_vertices: Numpy array-like, Vx3
         :param weighting_function: WeightingFunction
         """
-        self.rest_vertices = rest_vertices
         self.weighting_function = weighting_function
 
-    def apply_lbs(self, pose):
+    def apply_lbs(self, rest_vertices, pose):
         """
+
 
         Apply traditional LBS to the rest vertices according to some pose.
 
         :param pose: Pose, must have the same number of bones as self.weighting_function has weights
         :return: Numpy array-like, Vx3, the deformed vertices.
         """
-        result = np.zeros(self.rest_vertices.shape)
-        weights = self.weighting_function.evaluate_set(self.rest_vertices, pose)
+        result = np.zeros(rest_vertices.shape)
+        weights = self.weighting_function.evaluate_set(rest_vertices, pose)
 
-        for i in range(0, self.rest_vertices.shape[0]):
+        for i in range(0, rest_vertices.shape[0]):
 
-            vertex = self.rest_vertices[i]
+            vertex = rest_vertices[i]
             vertex_homo = np.zeros((4, 1))
             vertex_homo[0] = vertex[0]
             vertex_homo[1] = vertex[1]
@@ -44,7 +43,7 @@ class LBSMeshDeformer:
             bone_matrices = pose.bone_matrices
             transform_matrix = np.zeros((4, 4))
 
-            for j in range(0, vertex_weights.shape[0]):
+            for j in range(0, pose.bone_matrices.shape[0]):
 
                 transform_matrix += vertex_weights[j] * bone_matrices[j]
 
@@ -52,3 +51,38 @@ class LBSMeshDeformer:
             result[i] = vertex_trans[:3, 0]
 
         return result
+
+    def invert_lbs(self, rest_vertices, pose):
+        """
+
+
+
+        :param pose:
+        :return:
+        """
+        result = np.zeros(rest_vertices.shape)
+        weights = self.weighting_function.evaluate_set(rest_vertices, pose)
+
+        for i in range(0, rest_vertices.shape[0]):
+
+            vertex = rest_vertices[i]
+            vertex_homo = np.zeros((4, 1))
+            vertex_homo[0] = vertex[0]
+            vertex_homo[1] = vertex[1]
+            vertex_homo[2] = vertex[2]
+            vertex_homo[3] = 1
+
+            vertex_weights = weights[i]
+            bone_matrices = pose.inverse_bone_matrices
+            transform_matrix = np.zeros((4, 4))
+
+            for j in range(0, pose.inverse_bone_matrices.shape[0]):
+
+                transform_matrix += vertex_weights[j] * bone_matrices[j]
+
+            vertex_trans = np.matmul(transform_matrix, vertex_homo)
+            result[i] = vertex_trans[:3, 0]
+
+        return result
+
+
