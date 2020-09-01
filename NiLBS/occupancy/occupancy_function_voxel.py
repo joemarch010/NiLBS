4b1554b88e09ef70b8d1d3fa0fe9f1185fec2b58
jm360 <jm360@st-andrews.ac.uk>
2020-09-01 14:38:23 +0100

    Groundwork for initial NiLBS implementation.

--- NiLBS/occupancy/occupancy_function_voxel.py
+++ NiLBS/occupancy/occupancy_function_voxel.py
@@ -10,7 +10,16 @@ class OccupancyFunctionVoxel(OccupancyFunction):
 
 
     """
-    def __init__(self, voxel_grid):
+    def __init__(self, voxel_grid_file):
 
-        self.voxel_grid = voxel_grid
-        
+        self.voxel_grid = voxel_grid_file['voxel_gird']
+        self.voxel_start = voxel_grid_file['voxel_start']
+        self.voxel_dimensions = voxel_grid_file['voxel_dimensions']
+
+    def evaluate(self, x):
+
+        return None
+
+    def evaluate_set(self, X):
+
+        return None
