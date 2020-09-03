7025cc85ed5b7d43038ca7868db71b44f4d788ec
jm360 <jm360@st-andrews.ac.uk>
2020-09-03 15:16:58 +0100

    Packaging.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='NiLBS',
+   version='0.5',
+   description='NiLBS 3D implementation.',
+   author='Joe March',
+   author_email='jgm45@cam.ac.uk',
+   packages=['NiLBS'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
