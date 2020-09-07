83105b10fabd02d71f3e43c1277f17d8a625e50d
jm360 <jm360@st-andrews.ac.uk>
2020-09-07 10:04:39 +0100

    Removed  VPose dependencies.

--- demo/model_view.py
+++ demo/model_view.py
@@ -1,10 +1,7 @@
 import numpy as np
 import trimesh
+import pyrender
 
-from human_body_prior.tools.omni_tools import colors
-from human_body_prior.mesh import MeshViewer
-from human_body_prior.tools.omni_tools import copy2cpu as c2c
-from human_body_prior.body_model.body_model import BodyModel
 
 from NiLBS.weighting.weighting_function_pointwise import WeightingFunctionPointwise
 from NiLBS.weighting.weighting_function_mlp_naive import WeightingFunctionMLPNaive
@@ -12,28 +9,29 @@ from NiLBS.weighting.weighting_function_mlp_rest_naive import WeightingFunctionM
 from NiLBS.skinning.skinning_mesh_lbs import LBSDeformer
 from NiLBS.skinning.util import redistribute_weights
 from NiLBS.sampling.pose_sampler_amass import PoseSamplerAMASS
-
+from NiLBS.body.human_body import HumanBody
 
 bm_path = '../data/AMASS/body_models/smplh/female/model.npz'
 npz_bdata_path = '../data/AMASS/MPILimits/MPI_Limits/03099/lar1_poses.npz'
 weight_model_path_mlp = '../models/weight_naive'
 weight_model_path_mlpr = '../models/weight_rest_naive'
 
-bm = BodyModel(bm_path=bm_path)
-faces = c2c(bm.f)
+body_model_dict = np.load(bm_path)
+body_model = HumanBody(body_model_dict)
+joints = body_model_dict['J']
+faces = body_model.faces
 
 bdata = np.load(npz_bdata_path)
-psa = PoseSamplerAMASS(bm, bdata)
+psa = PoseSamplerAMASS(body_model, bdata)
 frame_poses = psa.sample_frames(step=100, n_frames=10)
 
 imw, imh=1000, 1000
-mv = MeshViewer(width=imw, height=imh, use_offscreen=False)
 
-weights = c2c(bm.weights)
-bone_hierarchy = c2c(bm.kintree_table)
+weights = body_model.weights
+bone_hierarchy = body_model.bone_hierarchy
 active_bones = range(0, 22)
 weights = redistribute_weights(weights, bone_hierarchy, active_bones)
-vertices = c2c(bm.v_template)[0]
+vertices = body_model.vertex_template
 
 #wfmlp = WeightingFunctionMLPNaive(model_path=weight_model_path_mlp)
 #wfmlpr = WeightingFunctionMLPRestNaive(model_path=weight_model_path_mlpr)
@@ -44,7 +42,7 @@ md_normal = LBSDeformer(wf)
 #md_mlp = LBSMeshDeformer(vertices, wfmlp)
 #md_mlpr = LBSMeshDeformer(vertices, wfmlpr)
 
-body_mesh_normal = trimesh.Trimesh(vertices=md_normal.apply_lbs(vertices, frame_poses[2]), faces=faces, vertex_colors=np.tile(colors['grey'], (6890, 1)))
+body_mesh_normal = trimesh.Trimesh(vertices=md_normal.apply_lbs(vertices, frame_poses[2]), faces=faces, vertex_colors=np.tile((1.0, 1.0, 0.4), (6890, 1)))
 #body_mesh_mlp = trimesh.Trimesh(vertices=md_mlp.apply_lbs(frame_poses[2]), faces=faces, vertex_colors=np.tile(colors['grey'], (6890, 1)))
 #body_mesh_mlpr = trimesh.Trimesh(vertices=md_mlpr.apply_lbs(frame_poses[0]), faces=faces, vertex_colors=np.tile(colors['grey'], (6890, 1)))
 
@@ -61,8 +59,17 @@ meshes.append(body_mesh_normal)
 
 print(len(meshes))
 
-mv.set_static_meshes(meshes)
-mv.set_background_color([0.3, 0.4, 0.9])
+
+scene = pyrender.Scene()
+
+mesh = pyrender.mesh.Mesh.from_trimesh(body_mesh_normal)
+scene.add(mesh)
+
+camera = pyrender.PerspectiveCamera(yfov=np.pi / 3.0)
+camera_pose = np.eye(4)
+camera_pose[:3, 3] = np.array([0, 0, 2.5])
+scene.add(camera, pose=camera_pose)
 
 
-mv.render(render_wireframe=False)
+viewer = pyrender.Viewer(scene, use_raymond_lighting=True, run_in_thread=False)
+viewer.run()
