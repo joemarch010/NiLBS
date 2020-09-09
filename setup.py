e0280966ec317994fe149565b9bb4bd4a9703873
jm360 <jm360@st-andrews.ac.uk>
2020-09-09 13:31:20 +0100

    Updated to 0.5.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='NiLBS',
-   version='0.5.9',
+   version='0.5.10',
    description='NiLBS 3D implementation.',
    author='Joe March',
    author_email='jgm45@cam.ac.uk',
