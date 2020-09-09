feff1c93c1c5425d5021988ff558b54148cc0e35
jm360 <jm360@st-andrews.ac.uk>
2020-09-09 12:55:20 +0100

    Added NiLBS demo.

--- NiLBS/demo/scene.py
+++ NiLBS/demo/scene.py
@@ -5,8 +5,12 @@ import numpy as np
 import os
 
 from NiLBS.body.human_body import HumanBody
+from NiLBS.occupancy.occupancy_function_mesh import OccupancyFunctionMesh
+from NiLBS.occupancy.occupancy_function_rest_cached import OccupancyFunctionRestCached
+from NiLBS.occupancy.voxel.util import extract_voxel_grid
 from NiLBS.sampling.pose_sampler_amass import PoseSamplerAMASS
 from NiLBS.skinning.skinning_mesh_lbs import LBSDeformer
+from NiLBS.weighting.weighting_function import WeightingFunction
 from NiLBS.weighting.weighting_function_pointwise import WeightingFunctionPointwise
 
 
@@ -39,5 +43,33 @@ def make_default_model_pose_scene():
     camera_pose = np.eye(4)
     camera_pose[:3, 3] = np.array([0, 0, 2.5])
     result.add(camera, pose=camera_pose)
+
+    return result
+
+
+def make_default_nilbs_scene():
+
+    result = pyrender.Scene()
+
+    weighting_function = WeightingFunction(body_model.weights.shape[0] + 1)
+    rest_trimesh_mesh = trimesh.Trimesh(vertices=body_model.vertex_template, faces=body_model.faces,
+                                        vertex_colors=np.tile((1.0, 1.0, 0.4), (6890, 1)))
+    rest_occupancy_function = OccupancyFunctionMesh(rest_trimesh_mesh)
+    posed_occupancy_function = OccupancyFunctionRestCached(rest_occupancy_function, pose, weighting_function)
+    voxel_dict = extract_voxel_grid(posed_occupancy_function, rest_trimesh_mesh.bounds, np.array([32, 32, 32]))
+    voxel_grid = voxel_dict['voxel_grid']
+    voxel_start = voxel_dict['voxel_start']
+    voxel_dimensions = voxel_dict['voxel_dimensions']
+
+    for i in range(0, voxel_grid.shape[0]):
+        for j in range(0, voxel_grid.shape[1]):
+            for k in range(0, voxel_grid.shape[2]):
+                if voxel_grid[i][j][k] > 0.5:
+                    voxel_position = voxel_start + voxel_dimensions * np.array([i, j, k])
+
+                    trimesh_mesh = trimesh.creation.box(voxel_dimensions,
+                                                        trimesh.transformations.translation_matrix(voxel_position))
+                    box_mesh = pyrender.Mesh.from_trimesh(trimesh_mesh)
+                    result.add(box_mesh)
 
     return result
--- demo/nilbs_demo.py
+++ demo/nilbs_demo.py
@@ -1,75 +1,11 @@
-import numpy as np
-import trimesh
-import copy
+import pyrender
 
-from human_body_prior.tools.omni_tools import colors
-from human_body_prior.mesh import MeshViewer
-from human_body_prior.tools.omni_tools import copy2cpu as c2c
-from human_body_prior.body_model.body_model import BodyModel
+from NiLBS.demo.scene import make_default_nilbs_scene
 
-from NiLBS.weighting.weighting_function import WeightingFunction
-from NiLBS.skinning.util import redistribute_weights
-from NiLBS.sampling.pose_sampler_amass import PoseSamplerAMASS
-from NiLBS.occupancy.occupancy_function_mesh import OccupancyFunctionMesh
-from NiLBS.occupancy.occupancy_function_rest_cached import OccupancyFunctionRestCached
-from NiLBS.occupancy.voxel.util import extract_voxel_grid
-
-
-bm_path = '../data/AMASS/body_models/smplh/female/model.npz'
-npz_bdata_path = '../data/AMASS/MPILimits/MPI_Limits/03099/lar1_poses.npz'
-
-bm = BodyModel(bm_path=bm_path)
-faces = c2c(bm.f)
-
-bdata = np.load(npz_bdata_path)
-psa = PoseSamplerAMASS(bm, bdata)
-frame_poses = psa.sample_frames(step=100, n_frames=10)
 
 imw, imh=1000, 1000
-mv = MeshViewer(width=imw, height=imh, use_offscreen=False)
 
-weights = c2c(bm.weights)
-bone_hierarchy = c2c(bm.kintree_table)
-active_bones = range(0, 22)
-weights = redistribute_weights(weights, bone_hierarchy, active_bones)
-vertices = c2c(bm.v_template)[0]
-wf = WeightingFunction(weights.shape[1] + 1)
-body_mesh_rest = trimesh.Trimesh(vertices=vertices, faces=faces, vertex_colors=np.tile(colors['grey'], (6890, 1)))
-rest_occupancy = OccupancyFunctionMesh(body_mesh_rest)
-posed_occupancy = OccupancyFunctionRestCached(rest_occupancy, frame_poses[0], wf)
-voxel_bounds = copy.copy(body_mesh_rest.bounds)
-t = voxel_bounds[0][1]
-voxel_bounds[0][1] = voxel_bounds[0][2] * 2
-voxel_bounds[0][2] = t
-voxel_bounds[0][0] *= 2
-t = voxel_bounds[1][1]
-voxel_bounds[1][1] = voxel_bounds[1][2] * 2
-voxel_bounds[1][2] = t
-voxel_bounds[1][0] *= 2
-voxel_dict = extract_voxel_grid(posed_occupancy, voxel_bounds, np.array([32, 32, 32]))
-voxel_grid = voxel_dict['voxel_grid']
-voxel_start = voxel_dict['voxel_start']
-voxel_dimensions = voxel_dict['voxel_dimensions']
+scene = make_default_nilbs_scene()
 
-print('Iso-Surface Extracted')
-
-meshes = []
-
-for i in range(0, voxel_grid.shape[0]):
-    for j in range(0, voxel_grid.shape[1]):
-        for k in range(0, voxel_grid.shape[2]):
-            if voxel_grid[i][j][k] > 0.5:
-
-                voxel_position = voxel_start + voxel_dimensions * np.array([i, j, k])
-
-                box = trimesh.creation.box(voxel_dimensions, trimesh.transformations.translation_matrix(voxel_position))
-                meshes.append(box)
-
-print('Meshes Added')
-
-print(len(meshes))
-
-mv.set_static_meshes(meshes)
-mv.set_background_color([0.3, 0.4, 0.9])
-
-mv.render(render_wireframe=False)
+viewer = pyrender.Viewer(scene, use_raymond_lighting=True, run_in_thread=False)
+viewer.run()
