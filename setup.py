c7fcc27ea71010cdc952eb5d5dd52173f933b448
jm360 <jm360@st-andrews.ac.uk>
2020-09-09 08:35:39 +0100

    Updated to v0.5.4.

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@ from setuptools import setup
 
 setup(
    name='NiLBS',
-   version='0.5.3',
+   version='0.5.4',
    description='NiLBS 3D implementation.',
    author='Joe March',
    author_email='jgm45@cam.ac.uk',
-   packages=['NiLBS.NiLBS'],  #same as name
+   packages=['NiLBS',
+             'NiLBS.body',
+             'NiLBS.mesh',
+             'NiLBS.occupancy',
+             'NiLBS.occupancy.voxel',
+             'NiLBS.pose',
+             'NiLBS.sampling',
+             'NiLBS.weighting'],
    install_requires=['trimesh', 'pyrender', 'tensorflow'], #external packages as dependencies
 )
